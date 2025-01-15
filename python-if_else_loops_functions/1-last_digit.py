0fa612b9ce34cabfec243bb32a6dc37c266e9c6d
HB10065 <10065@holbertonstudents.com>
2025-01-15 18:54:19 -0300

    arreglo de caso erroneo al numero ser menor a cero

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 ld = abs(number) % 10
-print(f"Last digit of {number} is {ld} and is ", end="")
+print(f"Last digit of {number} is ", end="")
+if number < 0:
+    ld = -ld
+print(f"{ld} and is ", end="")
 if ld > 5:
     print("greater than 5")
 elif ld == 0:
