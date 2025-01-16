1a72f5224eabb7250854ec9af4a04015edcb5514
HB10065 <10065@holbertonstudents.com>
2025-01-16 13:03:23 -0300

    .format()

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for a in str:
-        print(f'{chr(ord(a) - 32) if ord(a) >= ord('a') and a <= ord('z') else a}', end='')
+        print('{}'.format(chr(ord(a) - 32) if ord(a) >= ord('a') and a <= ord('z') else a), end='')
     print(f'')
