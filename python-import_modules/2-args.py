a41ccc076110786146142494914dfefba4417ce6
HB10065 <10065@holbertonstudents.com>
2025-01-22 11:05:21 -0300

    >=

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) - 1 > 1:
+    if len(sys.argv) - 1 >= 1:
         print('{} '.format(len(sys.argv) - 1), end='')
         print('argument:' if len(sys.argv) - 1 == 1 else 'arguments:')
         i = 1
