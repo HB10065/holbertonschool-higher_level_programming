1e2d2078aa6eb46281568e35a456c250ee51afa1
HB10065 <10065@holbertonstudents.com>
2025-01-22 09:43:20 -0300

    main

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-a = 10
-b = 5
-from calculator_1 import add, sub, mul, div
-print('10 + 5 = {}'.format(add(a, b)))
-print('10 - 5 = {}'.format(sub(a, b)))
-print('10 * 5 = {}'.format(mul(a, b)))
-print('10 / 5 = {}'.format(div(a, b)))
+def main():
+    a = 10
+    b = 5
+    from calculator_1 import add, sub, mul, div
+    print('10 + 5 = {}'.format(add(a, b)))
+    print('10 - 5 = {}'.format(sub(a, b)))
+    print('10 * 5 = {}'.format(mul(a, b)))
+    print('10 / 5 = {}'.format(div(a, b)))
