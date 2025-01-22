b63ecaae6e429bf486fe1aba4f5fda0f45ee9d67
HB10065 <10065@holbertonstudents.com>
2025-01-22 11:04:41 -0300

    todos los -1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) > 1:
-        print('{} '.format(len(sys.argv)), end='')
-        print('argument:' if len(sys.argv) == 1 else 'arguments:')
+    if len(sys.argv) - 1 > 1:
+        print('{} '.format(len(sys.argv) - 1), end='')
+        print('argument:' if len(sys.argv) - 1 == 1 else 'arguments:')
         i = 1
         for arg in sys.argv[1:]:
             print('{}: {}'.format(i, arg))
