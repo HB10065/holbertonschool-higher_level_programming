a25fbf7c8c51f0efe602ac243c073351a79dde78
HB10065 <10065@holbertonstudents.com>
2025-01-22 12:03:26 -0300

    [1:-1]

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         suma = int(sys.argv[1])
         i = 1
-        for arg in sys.argv[1:]:
+        for arg in sys.argv[1:-1]:
             suma += int(sys.argv[i + 1])
             i += 1
         print('{}'.format(suma))
