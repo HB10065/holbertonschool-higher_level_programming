9042d55f53cde2eecbd82a4ec9409a97bde30251
HB10065 <10065@holbertonstudents.com>
2025-01-22 12:01:48 -0300

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        suma = int(sys.argv[1])
+        i = 1
+        for arg in sys.argv[1:]:
+            suma += int(sys.argv[i + 1])
+            i += 1
+        print('{}'.format(suma))
+    else:
+        print('0')
