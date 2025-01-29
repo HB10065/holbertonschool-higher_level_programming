f5e35a8d5230fd903775fbd0a0f95e113572d221
HB10065 <10065@holbertonstudents.com>
2025-01-29 10:42:25 -0300

    +=

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,5 @@ def no_c(my_string):
     new_string = ''
     for i in my_string:
         if i != 'c' or i != 'C':
-            new_string[a] = i
-            a += 1
+            new_string += i
     return new_string
