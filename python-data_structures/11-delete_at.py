2a1739a715963cecbbc0410cae127fd99aa09f98
HB10065 <10065@holbertonstudents.com>
2025-01-29 14:56:35 -0300

    11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,5 +3,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx + 1 > len(my_list):
         return my_list
 
-    my_list = my_list[:idx - 1] + my_list[idx + 1:]
+    my_list = my_list[:idx] + my_list[idx:]
     return my_list
