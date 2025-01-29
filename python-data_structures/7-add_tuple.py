7d5c461f0f0b1fd5a4232493b5648c8f48297830
HB10065 <10065@holbertonstudents.com>
2025-01-29 13:31:18 -0300

    me complico solo

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif len(tuple_b) == 0 or len(tuple_a) == 0:
-        if len(tuple_a) == 0 and len(tuple_b) == 0:
-            new_tuple = (0, 0)
-        elif len(tuple_a) == 0:
-            new_tuple = (tuple_b[0], tuple_b[1])
-        else:
-            new_tuple = (tuple_a[0], tuple_a[1])
-    else:
-        if len(tuple_a) == 1 and len(tuple_b) == 1:
-            new_tuple = (tuple_a[0] + tuple_b[0], 0)
-        elif len(tuple_a) == 1:
-            new_tuple = (tuple_a[0] + tuple_b[0], tuple_b[1])
-        else:
-            new_tuple = new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1])
-    return new_tuple
+    a1, a2 = (tuple_a + (0, 0))[:2]
+    b1, b2 = (tuple_b + (0, 0))[:2]
+    return (a1 + b1, a2 + b2)
