15118a15a49fc7a0ab206e74369db077bdd55b32
HB10065 <10065@holbertonstudents.com>
2025-01-29 14:51:40 -0300

    11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -5,7 +5,7 @@ def delete_at(my_list=[], idx=0):
 
     for i in range(len(my_list)):
         if i == idx:
-            for j in range(i, len(my_list)):
+            for j in range(i, len(my_list) - 1):
                 my_list[j] = my_list[j + 1]
             my_list.remove(len(my_list) - 1)
             break
