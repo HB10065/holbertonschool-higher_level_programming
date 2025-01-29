36459dbbff06ce642e4621ac1a27a4dd38b3fd3c
HB10065 <10065@holbertonstudents.com>
2025-01-29 12:57:23 -0300

    7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len (tuple_a) == 0 or len(tuple_b) == 0:
+        new_tuple = (0, 0)
+        return new_tuple
+    if len(tuple_a) < 2 or len(tuple_b) < 2:
+        new_tuple = (tuple_a[0] + tuple_b[0], 0)
+        return new_tuple
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
