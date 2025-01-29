5c5409579dcebd680289053c07ae1ab0f80a9389
HB10065 <10065@holbertonstudents.com>
2025-01-29 12:17:14 -0300

    6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print(' ')
+        return
     for i in matrix:
         for j in range(len(i)):
             if j != len(i) - 1:
