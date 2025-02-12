acb05601a32acc7e4244b5d8469ab534115877ea
HB10065 <10065@holbertonstudents.com>
2025-02-12 15:04:31 -0300

    a

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Modulo
+'''
+
+
+from 5-save_to_json_file import save_to_json_file as savejson
+from 6-load_from_json_file import load_from_json_file as loadjson
+
+
+def main():
+    
