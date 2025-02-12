0855daf25ca960583db34a837cd8a353dd265efd
HB10065 <10065@holbertonstudents.com>
2025-02-12 14:34:20 -0300

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Modulo
+'''
+
+
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    Funcion
+    '''
+    with open(filename, 'r') as t:
+        return json.loads(t.read())
+    
