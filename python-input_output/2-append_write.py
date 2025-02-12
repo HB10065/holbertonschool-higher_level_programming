7da0ce07df006901ccc7bfd41695ea672065e53b
HB10065 <10065@holbertonstudents.com>
2025-02-12 09:33:16 -0300

    2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -8,5 +8,6 @@ def append_write(filename="", text=""):
     '''
     Funcion
     '''
-    with open(filename, 'w') as t:
-        t.a(text)
+    with open(filename, 'a') as t:
+        t.write(text)
+    return len(text)
