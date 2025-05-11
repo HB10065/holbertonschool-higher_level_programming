faa161b6aef58c09c39dedb7997a340c33dd5cf8
HB10065 <10065@holbertonstudents.com>
2025-05-11 20:50:07 -0300

    first print, spaces in end=

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number < 0:
     last_digit = number % -10
 else:
     last_digit = number % 10
-print("Last digit of {} is {} and is ".format(number, last_digit), end = '')
+print("Last digit of {} is {} and is ".format(number, last_digit), end='')
 
 if last_digit < 6 and last_digit != 0:
     print("less than 6 and not 0")
