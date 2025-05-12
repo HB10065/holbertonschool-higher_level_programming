4bcf1cc58e65187a7ee2b2f5a45b32239c0c8dd0
HB10065 <10065@holbertonstudents.com>
2025-05-11 23:17:11 -0300

    11 finished

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
-    c = a
-    for i in range(b):
-        c = c * c
-    return c
+    if b == 0:
+        return 1
+    else:
+        c = a
+        for i in range(1,b):
+            c = c * c
+        return c
