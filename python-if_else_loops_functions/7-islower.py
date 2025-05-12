f0d457a298754bbea97a548dd24459963be78ad6
HB10065 <10065@holbertonstudents.com>
2025-05-11 22:06:07 -0300

    7 finished

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 def islower(c):
     low = 0
+    upp = 0
     for i in range(ord('a'), ord('z') + 1):
         if c == chr(i):
             low += 1
-    if low == 1:
+    
+    for i in range(ord('A'), ord('Z') + 1):
+        if c == chr(i):
+            upp += 1
+    
+    if upp == 0 and low == 0:
+        return None
+    elif low == 1:
         return True
     else:
         return False
