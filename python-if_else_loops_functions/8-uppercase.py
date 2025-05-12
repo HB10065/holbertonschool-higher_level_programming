6eda469794dbd0df86cbc62306af10d7d727bfcf
HB10065 <10065@holbertonstudents.com>
2025-05-11 22:37:11 -0300

    8 finished

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if str[i] in "abcdefghijklmnopqrstvwxyz":
-            str[i] = ord(str[i]) + 33
+        if i in "abcdefghijklmnopqrstvwxyz":
+            i = ord(i) + 33
     print("{}".format(str))
