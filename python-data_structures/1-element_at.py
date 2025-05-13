b41072a69a363e5e68709e09db27203b175c71b9
HB10065 <10065@holbertonstudents.com>
2025-05-13 14:29:09 -0300

    line 6, + 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     
-    if len(my_list) - 1 != idx:
+    if len(my_list) != idx + 1:
         return None
     
     element = my_list[idx]
