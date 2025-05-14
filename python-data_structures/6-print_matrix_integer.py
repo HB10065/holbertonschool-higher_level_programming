3e6fb54e5dccf824049357605b25e28fc5eff10d
HB10065 <10065@holbertonstudents.com>
2025-05-14 13:51:29 -0300

    6: last print

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 0:
-        return None
-
     for row in matrix:
         rowlen = len(row)
         for column in row:
             if column == row[rowlen - 1]:
-                print("{:d}".format(column))
+                print("{:d}".format(column), end='')
             else:
                 print("{:d}".format(column), end=' ')
+        print()
