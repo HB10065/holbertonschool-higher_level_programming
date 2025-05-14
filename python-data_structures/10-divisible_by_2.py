095c36021af674d0b91caba749c2baa0e778f172
HB10065 <10065@holbertonstudents.com>
2025-05-14 15:44:38 -0300

    10: for if else ret

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for element in my_list:
+        if element % 2 == 0:
+            div_list = div_list + [True,]
+        else:
+            div_list = div_list + [False,]
+
+    return div_list
