8b1052e0e6efce254da0608e5f0be041f0b1d9f4
HB10065 <10065@holbertonstudents.com>
2025-05-14 15:58:29 -0300

    11: removed = in line 5

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     new_list = []
     i = 0
-    while i <= len(my_list):
+    while i < len(my_list):
         if i == idx:
             i += 1
             continue
