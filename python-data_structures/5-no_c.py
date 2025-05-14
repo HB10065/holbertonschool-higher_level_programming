97fcded8b7e15675ba2427ab2fe6a4ad6ae9d3b6
HB10065 <10065@holbertonstudents.com>
2025-05-14 12:17:21 -0300

    5: taba todo mal

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,8 +2,9 @@
 def no_c(my_string):
     new_str = ""
     idx = 0
-    while len(my_string) <= idx:
+    while len(my_string) > idx:
         if my_string[idx] == 'c' or my_string[idx] == 'C':
             idx += 1
         new_str = new_str + my_string[idx]
+        idx += 1
     return new_str
