0d9373f6123616e592a5af21d5107b67f6d91c6a
HB10065 <10065@holbertonstudents.com>
2025-05-14 11:18:34 -0300

    4: if return change

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list
-    if idx < 0 or len(new_list) <= idx:
+    if idx < 0 or len(my_list) <= idx:
         return my_list
 
     new_list[idx] = element
