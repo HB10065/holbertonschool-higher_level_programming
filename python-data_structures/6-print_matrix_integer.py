8fb038b9b7d31877012f2f8ea3c5b49105bcab08
HB10065 <10065@holbertonstudents.com>
2025-05-14 13:48:51 -0300

    6: if else

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        return None
+
     for row in matrix:
         rowlen = len(row)
         for column in row:
