b6d22b7b61fdb58823c3709d51627d31124ac274
HB10065 <10065@holbertonstudents.com>
2025-05-14 12:21:07 -0300

    5: agregamos el continue

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -5,6 +5,7 @@ def no_c(my_string):
     while len(my_string) > idx:
         if my_string[idx] == 'c' or my_string[idx] == 'C':
             idx += 1
+            continue
         new_str = new_str + my_string[idx]
         idx += 1
     return new_str
