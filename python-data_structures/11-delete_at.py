4b5ee8574d9fc2cf7d649b021adae83d185a0b17
HB10065 <10065@holbertonstudents.com>
2025-05-14 15:55:28 -0300

    11: last += and <= in while

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,9 +2,10 @@
 def delete_at(my_list=[], idx=0):
     new_list = []
     i = 0
-    while i < len(my_list):
+    while i <= len(my_list):
         if i == idx:
             i += 1
             continue
         new_list = new_list + my_list[i]
+        i += 1
     return new_list
