cbea544abf98455eec824c8deff360b04c0970b9
HB10065 <10065@holbertonstudents.com>
2025-05-20 11:07:20 -0300

    doc

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,6 +7,10 @@ Module Documentation
 class Square:
     '''
     Class that represents a square
+
+    Attributes:
+    size: a private attribute representing
+        the size of the square
     '''
     def __init__(self, size):
         self.__size = size
