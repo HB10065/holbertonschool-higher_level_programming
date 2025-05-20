da5d54811a2d1476b5e8c272d8aa327714ea7c80
HB10065 <10065@holbertonstudents.com>
2025-05-20 12:17:58 -0300

    4: both def size

---
+++ python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+Module Documentation
+'''
+
+
+class Square:
+    '''
+    Class that represents a square
+
+    Attributes:
+    size: a private attribute representing
+        the size of the square
+    '''
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
