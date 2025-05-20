e025a0467993f03a4d0895d2e9ff460f3e46b3ed
HB10065 <10065@holbertonstudents.com>
2025-05-20 12:07:03 -0300

    3: area

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Module Documentation
+'''
+
+
+class Square:
+    '''
+    Class that represents a square
+
+    Attributes:
+    size: a private attribute representing
+        the size of the square
+    '''
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        return self.size * self.size
