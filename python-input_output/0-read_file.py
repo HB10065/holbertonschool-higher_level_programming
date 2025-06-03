dc7856083d34e286ae76a0e4445df5ca7f5d4521
HB10065 <10065@holbertonstudents.com>
2025-06-03 09:20:18 -0300

    0: first commit

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module Documentation
+'''
+
+
+def read_file(filename=""):
+    '''
+    Function Documentation
+    '''
+
+    with open(filename, 'r') as file:
+        text = file.read()
+    print('{}'.format(text))
