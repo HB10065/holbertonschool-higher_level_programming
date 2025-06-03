60b719d5642002a4d257a19cd78dc2c7bf1db612
HB10065 <10065@holbertonstudents.com>
2025-06-03 11:14:03 -0300

    6: first commit

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Module Documentation
+'''
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    Function Documentation
+    '''
+
+    with open(filename, 'r') as file:
+        obj = json.loads(file)
+    return obj
