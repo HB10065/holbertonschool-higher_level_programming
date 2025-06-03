2f50af2c05c76d46676cfc79f221515a0757e347
HB10065 <10065@holbertonstudents.com>
2025-06-03 11:07:57 -0300

    5: first commit

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module Documentation
+'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''
+    Function Documentation
+    '''
+
+    with open(filename, 'w') as file:
+        json.dump(json.dumps(my_obj), file)
