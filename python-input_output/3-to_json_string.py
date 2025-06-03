c63beed6df6c906d6a789b012377f3751605be61
HB10065 <10065@holbertonstudents.com>
2025-06-03 10:52:59 -0300

    3: first commit

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Moudule Documentation
+'''
+
+
+import json
+
+def to_json_string(my_obj):
+    '''
+    Function Documentation
+    '''
+
+    new_json = json.dumps(my_obj)
+    return new_json
