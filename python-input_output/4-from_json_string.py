443da06bfa8a311ae635ce0dc9997aa6a65cfce9
HB10065 <10065@holbertonstudents.com>
2025-06-03 11:00:19 -0300

    4: first commit

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module Documentation
+'''
+import json
+
+
+def from_json_string(my_str):
+    '''
+    Function Documentation
+    '''
+
+    obj = json.loads(my_str)
+    return obj
