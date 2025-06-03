a6b1353a63d492bf11379523a5d473bb3491ab38
HB10065 <10065@holbertonstudents.com>
2025-06-03 09:33:43 -0300

    1:first commit and 5

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Module Documentation
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    Function Documentation
+    '''
+
+    with open(filename, 'w') as file:
+        file.write(text)
