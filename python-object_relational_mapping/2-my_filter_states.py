e983029c43782823be68c92fa5d38daba01d325d
HB10065 <10065@holbertonstudents.com>
2025-07-09 14:34:58 +0000

    2: added case sensitive

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     )
 
     cursor = s_con.cursor()
-    cursor.execute('SELECT * FROM states WHERE name = '
+    cursor.execute('SELECT * FROM states WHERE BINARY name = '
                    '\'{}\' ORDER BY states.id ASC'.format(sys.argv[4]))
     for row in cursor.fetchall():
         print(row)
