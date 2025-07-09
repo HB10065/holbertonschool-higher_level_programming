1e342181bf15d5b282412400046d599356286fc5
HB10065 <10065@holbertonstudents.com>
2025-07-09 14:04:59 +0000

    0: pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,11 +9,11 @@ import sys
 if __name__ == '__main__':
 
     s_con = MySQLdb.connect(
-        host = 'localhost',
-        user = sys.argv[1],
-        passwd = sys.argv[2],
-        db = sys.argv[3],
-        port = 3306
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
     )
 
     cursor = s_con.cursor()
