9ab4dd1a217a2b9698e157ece16516b3a31ae98c
HB10065 <10065@holbertonstudents.com>
2025-07-09 15:06:30 +0000

    4: id

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     cursor = s_con.cursor()
     cursor.execute('SELECT * FROM cities'
-                   'ORDER BY cities.id ASC')
+                   'ORDER BY id ASC')
     for row in cursor.fetchall():
         print(row)
     cursor.close
