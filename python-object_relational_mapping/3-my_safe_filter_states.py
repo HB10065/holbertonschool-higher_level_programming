eebc88e58165f1c5e5a3e921f95c656163c7b016
HB10065 <10065@holbertonstudents.com>
2025-07-09 14:49:57 +0000

    3: pycodestyle

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     cursor = s_con.cursor()
     cursor.execute('SELECT * FROM states WHERE BINARY name = '
-                   '%s ORDER BY states.id ASC',(sys.argv[4],))
+                   '%s ORDER BY states.id ASC', (sys.argv[4],))
     for row in cursor.fetchall():
         print(row)
     cursor.close
