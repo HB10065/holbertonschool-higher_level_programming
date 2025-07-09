c3393f711c1634e72bb8024b7f8374f7e513d5d3
HB10065 <10065@holbertonstudents.com>
2025-07-09 14:25:38 +0000

    1: line 21 indentation

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     cursor = s_con.cursor()
     cursor.execute('SELECT * FROM states WHERE BINARY name LIKE \'N%\''
-                ' ORDER BY states.id ASC')
+                   ' ORDER BY states.id ASC')
     for row in cursor.fetchall():
         print(row)
     cursor.close
