73310319fc23160031c30c563684258d85c981e5
HB10065 <10065@holbertonstudents.com>
2025-07-09 14:42:56 +0000

    3: first commit

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Module Doc
+'''
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+
+    s_con = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cursor = s_con.cursor()
+    cursor.execute('SELECT * FROM states WHERE BINARY name = '
+                   '\'{}\' ORDER BY states.id ASC'.format(sys.argv[4]))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close
+    s_con.close
