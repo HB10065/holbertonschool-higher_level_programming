8ee5d2fa4a908c466f170eee8255ec26c4032b73
HB10065 <10065@holbertonstudents.com>
2025-07-16 15:36:22 +0000

    3: family friendly

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,15 +1,18 @@
 const toggle = document.querySelector('#toggle_header')
 const header = document.querySelector('header')
+const hclasslist = header.classList
 
-if (header.getAttribute('class') != 'green' && header.getAttribute('class') !='red'){
-        header.setAttribute('class', 'green')
+if (hclasslist.contains('green') == false && hclasslist.contains('red') == false){
+    hclasslist.add('green')
 }
 
 toggle.addEventListener('click', function() {
-    if (header.getAttribute('class') == 'green'){
-        header.setAttribute('class', 'red')
+    if (hclasslist.contains('green')){
+        hclasslist.remove('green')
+        hclasslist.add('red')
     }
     else{
-        header.setAttribute('class', 'green')
+        hclasslist.remove('red')
+        hclasslist.add('green')
     }
 })
