26792cfe8c06dce0f2e2046af924cc75789515fd
HB10065 <10065@holbertonstudents.com>
2025-08-01 19:13:58 +0000

    9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Module Doc
+'''
+base_geoemtry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(base_geoemtry):
+    '''
+    Class Doc
+    '''
+    def __init__(self, width, height):
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return f'[Rectangle] {self.__width}/{self.__height}'
