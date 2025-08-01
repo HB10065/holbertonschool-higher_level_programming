fec0f1ffa258a79a9cd95cf1fbafe197cf15fdc0
HB10065 <10065@holbertonstudents.com>
2025-08-01 18:12:15 +0000

    2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module Doc
+'''
+
+
+def is_same_class(obj, a_class):
+    '''
+    Function Doc
+    '''
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
