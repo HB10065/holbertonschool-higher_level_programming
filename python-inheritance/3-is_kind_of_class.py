31911307c71e5b38936035c015f3887fb07a1bb5
HB10065 <10065@holbertonstudents.com>
2025-08-01 18:19:31 +0000

    3: isinstance()

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module Doc
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    Function Doc
+    '''
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
