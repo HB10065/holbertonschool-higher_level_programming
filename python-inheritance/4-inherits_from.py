60d07507fd70f21e44b7a6d945f5ce9dcc7e4b6f
HB10065 <10065@holbertonstudents.com>
2025-08-01 18:23:34 +0000

    4: issubclass(class, class)

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,7 +8,7 @@ def inherits_from(obj, a_class):
     '''
     Function Doc
     '''
-    if issubclass(obj, a_class):
+    if issubclass(type(obj), a_class):
         return True
     else:
         return False
