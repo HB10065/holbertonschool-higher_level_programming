e21a8ac314629be42366cfde0e230a0d9d9c05cc
HB10065 <10065@holbertonstudents.com>
2025-08-01 19:51:26 +0000

    01

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,55 @@
+'''
+Module Doc
+'''
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    '''
+    Class Doc
+    '''
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    '''
+    Class Doc
+    '''
+    def __init__(self, radius):
+        if radius < 0:
+            raise ValueError('radius must be grater or equal to zero')
+        
+        self.__radius = radius
+
+    def perimeter(self):
+        return 2 * math.pi * self.__radius
+    
+    def area(self):
+        return math.pi * (self.__radius ** 2)
+
+class Rectangle(Shape):
+    '''
+    Class Doc
+    '''
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def perimeter(self):
+        return 2 * self.__width + 2 * self.__height
+    
+    def area(self):
+        return self.__height * self.__width
+
+def shape_info(shape):
+    '''
+    Function Doc
+    '''
+    print(f'Area: {shape.area()}')
+    print(f'Perimeter: {shape.perimeter()}')
