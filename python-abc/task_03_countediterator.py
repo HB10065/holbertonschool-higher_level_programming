9a276f90dff43a69111b75d08158b5d670b6f7ab
HB10065 <10065@holbertonstudents.com>
2025-08-01 20:09:23 +0000

    03

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Module doc
+'''
+
+
+class CountedIterator():
+    '''
+    Class Doc
+    '''
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = next(self.iterator)
+        self.count += 1
+        return item
+
+    def get_count(self):
+        return self.count
