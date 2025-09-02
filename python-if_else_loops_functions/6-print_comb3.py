d503c6db49619077ccd4a23d8db21455bb030ad8
HB10065 <10065@holbertonstudents.com>
2025-09-02 10:30:10 -0300

    6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for num1 in range(8):
+    for num2 in range(num1, 10):
+        if num1 == num2:
+            continue
+        print('{}{}, '.format(num1, num2), end='')
+print('89')
