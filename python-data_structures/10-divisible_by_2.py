edd951bc881597247fec62a12f8b91661605a192
HB10065 <10065@holbertonstudents.com>
2025-09-09 14:20:10 -0300

    10: si

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,8 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for element in my_list:
         if element % 2 == 0:
-            new_list = new_list.append(True)
+            new_list.append(True)
         else:
-            new_list = new_list.append(False)
+            new_list.append(False)
 
     return new_list
