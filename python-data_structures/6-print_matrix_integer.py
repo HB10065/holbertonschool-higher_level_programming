b28dd3d55c95167fc59a41557af75f24a7148857
HB10065 <10065@holbertonstudents.com>
2025-09-09 13:45:35 -0300

    6: hardcodeado

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+
     for row in matrix:
         for column in range(len(row)):
             if column == len(row) - 1:
