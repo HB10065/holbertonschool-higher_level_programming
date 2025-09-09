132cc13162cc77a1a3378eff3a08a9bad6f356c7
HB10065 <10065@holbertonstudents.com>
2025-09-09 14:13:09 -0300

    9: first commit

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    else:
+        sorted_list = sorted(my_list, reverse=True)
+        return sorted_list[0]
