6303df06647e6c3f39fc315afa078601118addad
HB10065 <10065@holbertonstudents.com>
2025-09-11 12:44:15 -0300

    2: first commit

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    for element in my_list:
+        if element not in new_list:
+            new_list.append(element)
+    
+    for element2 in new_list:
+        num += element2
+    
+    return num
