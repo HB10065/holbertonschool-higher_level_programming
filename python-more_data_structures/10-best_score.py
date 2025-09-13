90098fcae0975f998f250d25dea7822ecf6f2ea9
HB10065 <10065@holbertonstudents.com>
2025-09-13 11:37:44 -0300

    10: pycodestyle

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == {} or a_dictionary == None:
+    if a_dictionary == {} or a_dictionary is None:
         return None
 
-    dict_tuple = sorted(a_dictionary.items(), 
+    dict_tuple = sorted(a_dictionary.items(),
                         key=lambda item: item[1], reverse=True)
     return dict_tuple[0][0]
