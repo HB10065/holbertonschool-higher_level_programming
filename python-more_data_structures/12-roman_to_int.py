e4965358c9a6b8c730ac903a04ba62ef41432732
HB10065 <10065@holbertonstudents.com>
2025-09-13 13:47:35 -0300

    12: first commit

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+
+    roman_numbers = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_num = 0
+    for c in reversed(roman_string):
+        num = roman_numbers[c]
+        if num < prev_num:
+            total = total - num
+        else:
+            total = total + num
+        prev_num = num
+
+    return total
