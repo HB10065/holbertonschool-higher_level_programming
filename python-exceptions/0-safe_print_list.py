4fe71e0f044baa4c01d515caf9e0b1b2fc4bd89a
HB10065 <10065@holbertonstudents.com>
2025-09-13 14:41:14 -0300

    0: chaged for

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,8 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     try:
         total_prints = 0
-        for element in my_list:
-            print('{}'.format(element), end='')
+        for i in range(x):
+            print('{}'.format(my_list[i]), end='')
             total_prints += 1
         print()
         return total_prints
