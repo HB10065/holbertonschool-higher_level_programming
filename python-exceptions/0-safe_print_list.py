99fe8c9747ff49c5135f9799103a30fcaa566ad9
HB10065 <10065@holbertonstudents.com>
2025-09-13 14:39:21 -0300

    0: end= and new line

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         total_prints = 0
         for element in my_list:
-            print('{}'.format(element))
+            print('{}'.format(element), end='')
             total_prints += 1
+        print()
         return total_prints
     except IndexError:
         print('x cannot be larger than the list')
