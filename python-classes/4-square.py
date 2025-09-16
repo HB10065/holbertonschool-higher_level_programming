f044f1b40c5b77800ec0f130a00c2fcc1f02595f
HB10065 <10065@holbertonstudents.com>
2025-09-16 09:59:03 -0300

    4: moved validators to the setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,10 +9,6 @@ class Square():
     empty class
     '''
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
         self.__size = size
 
     @property
@@ -23,6 +19,10 @@ class Square():
     @size.setter
     def size(self, value):
         '''setter of size'''
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = value
 
     def area(self):
