49b43d280e5ccdf64f39e25e3b53ab07af10f832
HB10065 <10065@holbertonstudents.com>
2025-09-16 09:56:34 -0300

    4: first commit

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+This module contains an empty class
+'''
+
+
+class Square():
+    '''
+    empty class
+    '''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    @property
+    def size(self):
+        '''getter of size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''setter of size'''
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
