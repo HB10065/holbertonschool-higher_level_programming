87afac599dd97f124a2e0ca0fafe7e32b813f900
HB10065 <10065@holbertonstudents.com>
2025-09-17 00:30:33 -0300

    1: first commit

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+This is a module with the class Rectangle
+based on the contents of 0-rectangle.py
+'''
+
+
+class Rectangle():
+    '''
+    Class Rectangle
+    private Attributes: width, height
+    '''
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
