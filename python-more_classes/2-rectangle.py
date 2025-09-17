6c2bd8d51af200f00da34a0eee6913282eb1184e
HB10065 <10065@holbertonstudents.com>
2025-09-17 01:04:43 -0300

    2: fixed perimeter when width and/or height are 0

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -47,4 +47,7 @@ class Rectangle():
         return self.__width * self.__height
 
     def perimeter(self):
-        return (self.__height + self.__width) * 2
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__height + self.__width) * 2
