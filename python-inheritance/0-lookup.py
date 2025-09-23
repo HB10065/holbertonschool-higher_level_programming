b3b16ce772af072d127b73845f532ea4faafdff3
HB10065 <10065@holbertonstudents.com>
2025-09-23 10:10:23 -0300

    documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+'''
+This module has the lookup function
+'''
+
+
 def lookup(obj):
+    '''
+    argument: any object
+    return: a list of all avaliable attributes
+            and methods of the object
+    '''
     return dir(obj)
