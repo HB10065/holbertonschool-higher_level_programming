9bb713990e9d148c17acb3afd5e58da931c12420
HB10065 <10065@holbertonstudents.com>
2025-09-23 12:18:26 -0300

    added documentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+'''
+Module Doc
+'''
 def is_same_class(obj, a_class):
+    '''
+    Function is_same_class
+    Arguments:
+        obj: any object
+        a_class: any Class
+    return: True if obj is an instance of a_class
+            False otherwise
+    '''
     return isinstance(obj, a_class)
