6e79e81e18702592a0b0cd188a513d3a4b12935f
HB10065 <10065@holbertonstudents.com>
2025-09-26 12:52:16 -0300

    6: first commit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This moduel contains a class based off of '5-base_geometry'
+'''
+
+
+class BaseGeometry():
+    '''
+    Public Instance Methods:
+        area: not implemented yet
+    '''
+    # Methods
+    def area(self):
+        raise Exception('area() is not implemented')
