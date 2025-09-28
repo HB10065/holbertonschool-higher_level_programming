1afd4f4b55c4765f5d70241aa9524bb1401b69bc
HB10065 <10065@holbertonstudents.com>
2025-09-28 20:58:39 -0300

    7: first commit w/o tests

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+This moduel contains a class based off of '6-base_geometry'
+'''
+
+
+class BaseGeometry():
+    '''
+    Public Instance Methods:
+        area: not implemented yet
+    '''
+    # Methods
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
