7d27f1255c1a211667e5239c5d9269f75c4f6410
HB10065 <10065@holbertonstudents.com>
2025-10-05 20:30:11 -0300

    11: first commit

---
+++ python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+Module Doc
+'''
+
+
+class Student():
+    '''Class'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    # Methods
+    def to_json(self, attrs=None):
+        if (
+            isinstance(attrs, list) and
+            all(isinstance(element, str) for element in attrs)
+        ):
+            attributes = {
+                key: getattr(self, key) for key in attrs
+                if hasattr(self, key)
+            }
+            return attributes
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
