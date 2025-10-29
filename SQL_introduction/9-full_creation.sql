be8df4de79d5c3c5a37316a70dff7caef92db2ee
HB10065 <10065@holbertonstudens.com>
2025-10-29 09:55:53 -0700

    9: first commit

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- CREATE antoher table named second_table
+CREATE TABLE IF NOT EXISTS second_table(
+    id int,
+    name VARCHAR(256),
+    score int
+);
+
+INSERT INTO second_table
+VALUES (1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
