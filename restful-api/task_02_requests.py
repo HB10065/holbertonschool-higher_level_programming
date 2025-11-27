05ce5bd91ea95fb9ed15370db3eca44428477cd1
HB10065 <10065@holbertonstudens.com>
2025-11-27 08:15:05 -0800

    task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''Modulo'''
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+
+    print(f'Status Code: {response.status_code}')
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+def fetch_and_save_posts():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+        post_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+
+        with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
+              fieldname = ['id', 'title', 'body']
+              writer = csv.DictWriter(file, fieldnames=fieldname)
+              writer.writeheader()
+              writer.writerows(post_data)
