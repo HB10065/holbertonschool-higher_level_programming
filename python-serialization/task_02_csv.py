c81bc7e55ca22504f3ce6163696ab971df838e5b
HB10065 <10065@holbertonstudens.com>
2025-11-27 06:40:23 -0800

    csv

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Module Doc'''
+import json, csv
+
+
+def convert_csv_to_json(file_name):
+    '''csv ro json'''
+    try:
+        with open(file_name, newline='') as csv_file:
+            reader = csv.DictReader(csv_file)
+            data = list(reader)
+
+        with ('data.json', 'w')as json_file:
+            json.dump(data, json_file)
+    
+        return True
+    
+    except Exception:
+        return False
