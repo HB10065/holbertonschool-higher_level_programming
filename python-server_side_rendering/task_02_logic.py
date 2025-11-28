5f383e1eccec0217e826139b9cf7ca184919e3c9
HB10065 <10065@holbertonstudens.com>
2025-11-28 10:55:30 -0800

    task 2

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,20 @@
+import json
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/items')
+def items():
+    try:
+        with open('items.json') as f:
+            data = json.load(f)
+            items_list = data.get('items', [])
+    except Exception as e:
+        print(f"Error leyendo items.json: {e}")
+        items_list = []
+
+    return render_template('items.html', items=items_list)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
