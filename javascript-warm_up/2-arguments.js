ec2e3aceb4498ada215181f57adb344776981498
HB10065 <10065@holbertonstudens.com>
2025-11-28 12:38:50 -0800

    all tasks

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const {argv} = require('node:process');
+const argumentsLength = argv.length;
+
+if (argumentsLength === 2) {
+  console.log('No argument');
+} else if (argumentsLength === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const {argv} = require('node:process');
+const repeatTimes = parseInt(argv[2]);
+const phrase = 'C is fun';
+
+if (isNaN(repeatTimes)) {
+  console.log('Missing number of occurrences');
+} else {
+  let count = 0;
+  while (count < repeatTimes) {
+    console.log(phrase);
+    count++;
+  }
+}
